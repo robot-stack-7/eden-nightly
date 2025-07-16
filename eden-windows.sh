205e4317f4a4fa8563a71e8fc380a948ed80638a
Escary <81011361+pflyly@users.noreply.github.com>
2025-07-16 06:08:36 +0100

    bundle every needed dlls for ffmpeg arm64 (#95)

--- eden-windows.sh
+++ eden-windows.sh
@@ -15,10 +15,8 @@ if [[ "${ARCH}" == "ARM64" ]]; then
 # use prebuilt arm64 ffmpeg from https://github.com/tordona/ffmpeg-win-arm64/releases
 # trimmed unused files according to the ffmpeg x64 build from eden repo
 sed -i 's|set(package_base_url "https://github.com/eden-emulator/")|set(package_base_url "https://github.com/pflyly/eden-nightly/")|' CMakeModules/DownloadExternals.cmake
-sed -i '/if *(WIN32)/,/^elseif/ {
-    s|set(package_repo ".*")|set(package_repo "raw/refs/heads/main/")|
-    s|set(package_extension ".*")|set(package_extension ".zip")|
-}' CMakeModules/DownloadExternals.cmake
+sed -i 's|set(package_repo "ext-windows-bin/raw/master/")|set(package_repo "raw/refs/heads/main/")|' CMakeModules/DownloadExternals.cmake
+sed -i 's|set(package_extension ".7z")|set(package_extension ".zip")|' CMakeModules/DownloadExternals.cmake
 
 # Adapt upstream WIP changes
 sed -i '
@@ -69,6 +67,13 @@ ccache -s -v
 EXE_PATH=./bin/eden.exe
 
 if [[ "${ARCH}" == "ARM64" ]]; then
+	# Accoding to #89, we may need every dll for the prebuilt ffmpeg
+ 	wget -q https://github.com/tordona/ffmpeg-win-arm64/releases/download/7.1.1/ffmpeg-7.1.1-essentials-shared-win-arm64.7z -O ffmpeg-full.7z
+  	mkdir -p ffmpeg
+   	7z x ffmpeg-full.7z -offmpeg > /dev/null
+    	find ffmpeg -type f -iname "*.dll" -exec cp -v {} ./bin/ \;
+     	rm -rf ffmpeg ffmpeg-full.7z
+     	
   	# Use ARM64-specific Qt paths with windeployqt
  	"D:/a/eden-nightly/Qt/6.9.1/msvc2022_64/bin/windeployqt6.exe" --qtpaths "D:/a/eden-nightly/Qt/6.9.1/msvc2022_arm64/bin/qtpaths6.bat" --release --no-compiler-runtime --no-opengl-sw --no-system-d3d-compiler --no-system-dxc-compiler --dir bin "$EXE_PATH"
 else
