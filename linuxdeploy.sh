05947f1f63848b6982a9f3a24fdf95c046ad97b5
Escary <81011361+pflyly@users.noreply.github.com>
2025-08-22 21:09:56 +0100

    refactor and simplify linuxdeploy

--- linuxdeploy.sh
+++ linuxdeploy.sh
@@ -8,13 +8,7 @@ export ARCH=$(uname -m)
 BUILD_DIR=$(realpath "$1")
 APPDIR="${BUILD_DIR}/light/AppDir"
 
-mkdir -p "${APPDIR}/usr/bin"
 cd "${BUILD_DIR}"
-
-cp -v ../dist/org.eden_emu.eden.desktop "${APPDIR}"
-cp -v ../dist/org.eden_emu.eden.svg "${APPDIR}"
-cp -v ./bin/eden "${APPDIR}/usr/bin"
-ln -sfv ./org.eden_emu.eden.svg "${APPDIR}/.DirIcon"
 
 # Prepare linuxdepoly
 curl -fsSLo ./linuxdeploy "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-${ARCH}.AppImage"
@@ -31,11 +25,15 @@ export QT_QPA_PLATFORM="wayland;xcb"
 export EXTRA_PLATFORM_PLUGINS="libqwayland-egl.so;libqwayland-generic.so;libqxcb.so"
 export EXTRA_QT_MODULES="svg;waylandcompositor"
 
-# start to deploy
-NO_STRIP=1 ./linuxdeploy --appdir "${APPDIR}" --plugin qt --plugin checkrt
+# start to deploy into AppDir
+# bundle libSDL3 for steamdeck
+NO_STRIP=1 ./linuxdeploy \
+  --appdir "${APPDIR}" \
+  -e ./bin/eden \
+  -d ../dist/org.eden_emu.eden.desktop \
+  -i ../dist/org.eden_emu.eden.svg \
+  -l /usr/lib/libSDL3.so* \
+  --plugin qt \
+  --plugin checkrt
 
-# remove libvulkan because it causes issues with gamescope
-rm -fv ./light/AppDir/usr/lib/libvulkan.so*
-
-# Bundle libsdl3 to AppDir, needed for steamdeck
-cp /usr/lib/libSDL3.so* ./light/AppDir/usr/lib/
+ln -sfv "usr/share/icons/hicolor/scalable/apps/org.eden_emu.eden.svg" "${APPDIR}/.DirIcon"
