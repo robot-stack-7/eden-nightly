b8ba5a2577e19a004ac957c6ccb7ee45b5def682
Escary <81011361+pflyly@users.noreply.github.com>
2025-10-01 22:10:31 +0100

    disable sccache for android (#164)

--- eden-android.sh
+++ eden-android.sh
@@ -1,14 +1,6 @@
 #!/bin/bash -ex
 
-export NDK_CCACHE=$(which sccache)
-
 cd ./eden
-
-# don't build tests and build real release type
-sed -i '/"-DYUZU_ENABLE_LTO=ON"/a\
-                    "-DCMAKE_C_COMPILER_LAUNCHER=sccache",\
-                    "-DCMAKE_CXX_COMPILER_LAUNCHER=sccache",
-' src/android/app/build.gradle.kts
 
 if [ "$TARGET" = "Coexist" ]; then
     # Change the App name and application ID to make it coexist with official build
@@ -29,17 +21,11 @@ APK_NAME="Eden-${COUNT}-Android-${TARGET}"
 cd src/android
 chmod +x ./gradlew
 if [ "$TARGET" = "Optimised" ]; then
-	./gradlew assembleGenshinSpoofRelease --parallel --console=plain --info
+	./gradlew assembleGenshinSpoofRelease
 else
-	./gradlew assembleMainlineRelease --parallel --console=plain --info
+	./gradlew assembleMainlineRelease
 fi
-
-sccache -s
 
 APK_PATH=$(find app/build/outputs/apk -type f -name "*.apk" | head -n 1)
-if [ -z "$APK_PATH" ]; then
-    echo "Error: APK not found in expected directory."
-    exit 1
-fi
 mkdir -p artifacts
 mv "$APK_PATH" "artifacts/$APK_NAME.apk"
