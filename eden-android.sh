4623effe2526f4546b019e31242c835fe65bde8a
Escary <81011361+pflyly@users.noreply.github.com>
2025-10-02 11:34:04 +0100

    add android pgo use build

--- eden-android.sh
+++ eden-android.sh
@@ -3,12 +3,18 @@
 cd ./eden
 
 if [ "$TARGET" = "pgogen" ]; then
+	# patch to generate profraw
 	git apply ../patches/android_pgo_gen.patch
+fi
+
+if [ "$OPTIMIZE" = "PGO" ]; then
+	# pacth to use prfodata
+	git apply ../patches/android_pgo_use.patch
 fi
 
 if [ "$TARGET" = "Coexist" ]; then
     # Change the App name and application ID to make it coexist with official build
-    sed -i 's/applicationId = "dev\.eden\.eden_emulator"/applicationId = "dev.eden.eden_emulator.nightly"/' src/android/app/build.gradle.kts
+    sed -i 's/applicationId = "dev\.eden\.eden_emulator"/applicationId = "dev.eden_nightly.eden_emulator"/' src/android/app/build.gradle.kts
     sed -i 's/resValue("string", "app_name_suffixed", "Eden")/resValue("string", "app_name_suffixed", "Eden Nightly")/' src/android/app/build.gradle.kts
 fi        
 
@@ -18,7 +24,12 @@ if [ "$TARGET" = "Optimised" ]; then
 fi 
 
 COUNT="$(git rev-list --count HEAD)"
-APK_NAME="Eden-${COUNT}-Android-${TARGET}"
+
+if [ "$OPTIMIZE" = "PGO" ]; then
+	APK_NAME="Eden-${COUNT}-Android-${TARGET}-${OPTIMIZE}"
+else
+	APK_NAME="Eden-${COUNT}-Android-${TARGET}"
+fi
 
 cd src/android
 chmod +x ./gradlew
