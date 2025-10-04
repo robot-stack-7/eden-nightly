86710a1ec24f4fd6ae87525b3239f297d2ff6cb9
Escary <81011361+pflyly@users.noreply.github.com>
2025-10-04 18:13:37 +0100

    unzip and merge

--- eden-android.sh
+++ eden-android.sh
@@ -12,8 +12,9 @@ if [ "$OPTIMIZE" = "PGO" ]; then
 	git apply ../patches/android_pgo_use.patch
 
 	# merge profraw files with the same version ndk to avoid mismatch
+	unzip ../pgo/android.zip -d ../pgo
 	/usr/local/lib/android/sdk/ndk/28.2.13676358/toolchains/llvm/prebuilt/linux-x86_64/bin/llvm-profdata \
-    merge -o ../pgo/android.profdata ../pgo/*.profraw
+    merge -o ../pgo/android.profdata ../pgo/eden-*.profraw/*
 fi
 
 if [ "$TARGET" = "Coexist" ]; then
